03628c0ce5b5e492be143bc107c5fb6efbb26515
Piyush Pawar <piyushpawar199954@gmail.com>
2023-06-12 12:17:37 +0530

    Animation added

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
+// Torus
+
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({
   color: 0xff6347,
@@ -29,17 +31,20 @@ const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
+// Lights
+
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+// Helpers
+//const lightHelper = new THREE.PointLightHelper(pointLight);
+//const gridHelper = new THREE.GridHelper(200, 50);
+//scene.add(lightHelper, gridHelper);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+//const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -56,16 +61,50 @@ function addStar() {
 Array(200).fill().forEach(addStar);
 
 //Background
-const spaceTexture = new THREE.TextureLoader().load('space.png');
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
 //Avatar
-const piyushTexture = new THREE.TextureLoader().load('piyush.png');
+const piyushTexture = new THREE.TextureLoader().load('Piyush.png');
 const piyush = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
   new THREE.MeshBasicMaterial({ map: piyushTexture })
 );
 scene.add(piyush);
+
+//moon
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture })
+);
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.x = -10;
+
+piyush.position.z = -5;
+piyush.position.x = 2;
+
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  piyush.rotation.y += 0.01;
+  piyush.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 function animate() {
   requestAnimationFrame(animate);
@@ -73,7 +112,7 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  controls.update();
+  //controls.update();
 
   renderer.render(scene, camera);
 }
